b9001627961791e6e6fb7613de68337898583b3c
Usulidia <lidia@example.com>
2021-04-13 11:23:45 +0200

    Clase cambio2 en nuevorepositorio

---
+++ prueba1/src/cambio1/cambio2.java
@@ -0,0 +1,8 @@
+package cambio1;
+
+public class cambio2 {
+	public static void main (String []args) {
+		System.out.println("Clase cambio2 en nuevorepositorio");
+		System.out.println("Clase cambio2 en nuevorepositorio");
+	}
+}
